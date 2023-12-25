f9ecaa4073a61fa23dc4381fde4d12a8eb9d9a1a
Parashar Das <porashorbabu@gmail.com>
2023-12-26 00:15:22 +0600

    after ending

---
+++ bomtry/position.js
@@ -0,0 +1,44 @@
+let screeen = document.querySelector('.head');
+let screeen1 = document.querySelector('.head1');
+let screeen2 = document.querySelector('.head2');
+let screeen3 = document.querySelector('.head3');
+let screeen4 = document.querySelector('.head4');
+let screeen5 = document.querySelector('.head5');
+let screeen6 = document.querySelector('.head6');
+let screeen7 = document.querySelector('.head7');
+let screeen8 = document.querySelector('.head8');
+let screeen9 = document.querySelector('.head9');
+
+screeen.innerHTML = window.screen.availWidth;
+screeen1.innerHTML = window.screen.availHeight;
+screeen3.innerHTML = window.screen.colorDepth;
+screeen4.innerHTML = window.screen.height;
+screeen5.innerHTML = window.screen.pixelDepth;
+screeen6.innerHTML = window.screen.width;
+screeen7.innerHTML = window.screen.document;
+
+
+let location1 = document.querySelector('.location1');
+let location2 = document.querySelector('.location2');
+let location3 = document.querySelector('.location3');
+let location4 = document.querySelector('.location4');
+let location5 = document.querySelector('.location5');
+let location6 = document.querySelector('.location6');
+let location7 = document.querySelector('.location7');
+let location8 = document.querySelector('.location8');
+let location9 = document.querySelector('.location9');
+let location10 = document.querySelector('.location10');
+
+location1.innerHTML = window.location;
+location2.innerHTML = window.location.hostname;
+location3.innerHTML = window.location.host;
+location4.innerHTML = window.location.pathname;
+location5.innerHTML = window.location.port;
+location6.innerHTML = window.location.protocol;
+location7.innerHTML = window.location.assign();
+location8.innerHTML = window.location.reload();
+
+
+
+
+
